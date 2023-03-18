3b424d23784b76947f05d14ec57469c869b60f2b
Joseph Wachira <washywachira@gmail.com>
2023-03-18 12:37:46 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    let words = tutorial.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  });
 }
+
+describe('index.js', function () {
+  describe('titleCased()', function () {
+    it('returns an array with title case tutorial names', function () {
+      expect(titleCased()).to.have.all.members(
+        [
+          "What Does The This Keyword Mean?",
+          "What Is The Constructor OO Pattern?",
+          "Implementing Blockchain Web API",
+          "The Test Driven Development Workflow",
+          "What Is NaN And How Can We Check For It",
+          "What Is The Difference Between StopPropagation And PreventDefault?",
+          "Immutable State And Pure Functions",
+          "What Is The Difference Between == And ===?",
+          "What Is The Difference Between Event Capturing And Bubbling?",
+          "What Is JSONP?"
+        ]
+      )
+    });
+  });
+});
